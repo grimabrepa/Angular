7f82966ea82f2dfebdaa4e0f1ad6bd26a4be29b5
grimabrepa <ezcajo41@hotmail.com>
2022-03-02 17:18:42 -0300

    Interpolacion y directivas ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Marcelo';// interpolacion utilizada  en la app.component.html linea 1 y 2
+  titulo = 'Practica de Angular';// interpolacion utilizada  en la app.component.html linea 1 
   url = "https://static2.abc.es/media/bienestar/2021/07/06/helados-saludables-kCwD--1200x630@abc.jpg";// interpolacion utilizada  en la app.component.html linea 4
   gustos = ["Vainilla", "Frutilla", "Limon", "Chocolate"];// interpolacion utilizada  en la app.component.html linea 7
+  nombre!:string;//interpolacion utilizada  en la app.component.html linea 2 y 3
+  precios =[150, 300, 500, 750, 900, 1200]
 }
