caecaeee918ac2ef84fcc30552cba993b690a090
marcelo <marcelo@example.com>
2022-03-02 16:33:15 -0300

    Interpolacion y directivas ngModel,*ngFor, *ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'micode';
+  titulo = 'Marcelo';// interpolacion utilizada  en la app.component.html linea 1 y 2
+  url = "https://static2.abc.es/media/bienestar/2021/07/06/helados-saludables-kCwD--1200x630@abc.jpg";// interpolacion utilizada  en la app.component.html linea 4
+  gustos = ["Vainilla", "Frutilla", "Limon", "Chocolate"];// interpolacion utilizada  en la app.component.html linea 7
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';//Importacion del Modulo FormsModule utilizado en la linea 14
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, /** Se llamo al modulo FormsModule para que funcione el la directiva ngModel en el app.componente.html linea 2 */
   ],
   providers: [],
   bootstrap: [AppComponent]
